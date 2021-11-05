4a4184190186846d2050308ac9d1e1dff126ff53
Emil Bay <github@tixz.dk>
2021-11-05 11:11:21 +0100

    Add type annotations and docs

--- example.mjs
+++ example.mjs
@@ -1,7 +1,9 @@
-import { VegaWallet, HARDENED } from './dist/wallet.js'
+import { VegaWallet, HARDENED } from '@vegaprotocol/crypto'
 const mnemonic = 'swing ceiling chaos green put insane ripple desk match tip melt usual shrug turkey renew icon parade veteran lens govern path rough page render'
 
-const wallet = await VegaWallet.fromMnemonic(mnemonic)
-const kp = await wallet.keyPair(HARDENED + 0)
+;(async () => {
+  const wallet = await VegaWallet.fromMnemonic(mnemonic)
+  const kp = await wallet.keyPair(HARDENED + 0)
 
-console.log(JSON.stringify(kp))
+  console.log(JSON.stringify(kp))
+})()
