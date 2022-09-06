3bc9ad654f70ecc68712d488d2b80c2caf7475b9
Emil Bay <github@tixz.dk>
2022-09-06 16:47:20 +0200

    Add PoW

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -38,6 +38,39 @@ pub fn verify(signature: &[u8], message: &[u8], public_key: &[u8]) -> bool {
     };
 }
 
+#[wasm_bindgen]
+pub fn pow_hash(block_hash: &[u8], tid: &[u8], nonce: u64) -> Box<[u8]> {
+    let mut sha3 = Sha3::v256();
+    let mut digest = [0u8; 32];
+    sha3.update(b"Vega_SPAM_PoW");
+    sha3.update(block_hash);
+    sha3.update(tid);
+    sha3.update(&nonce.to_be_bytes());
+    sha3.finalize(&mut digest);
+
+    return Box::new(digest);
+}
+
+#[wasm_bindgen]
+pub fn pow_solve(difficulty: u32, block_hash: &[u8], tid: &[u8], start_nonce: u64) -> u64 {
+    let mut nonce: u64 = start_nonce;
+
+    loop {
+        let digest = pow_hash(block_hash, tid, nonce);
+
+        let x = u64::from_be_bytes([
+            digest[0], digest[1], digest[2], digest[3],
+            digest[4], digest[5], digest[6], digest[7]
+        ]);
+
+        if x.leading_zeros() >= difficulty {
+            return nonce;
+        }
+
+        nonce += 1;
+    }
+}
+
 fn _hash(message: &[u8]) -> [u8; 32] {
     let mut sha3 = Sha3::v256();
     let mut digest = [0u8; 32];
