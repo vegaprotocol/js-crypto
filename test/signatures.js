809aa86a8141dc96de6a1314e3371d9fd68f3abf
Emil Bay <github@tixz.dk>
2022-09-21 16:52:47 +0200

    Tiny signing test

---
+++ test/signatures.js
@@ -0,0 +1,13 @@
+import { VegaWallet, HARDENED } from '@vegaprotocol/crypto/vega-wallet'
+import { string, toHex } from '@vegaprotocol/crypto/buf'
+import test from 'tape'
+
+test('Vectors', async (assert) => {
+  const wallet = await VegaWallet.fromMnemonic('...')
+  const keys = await wallet.keyPair(HARDENED + 0)
+
+  const sig = await keys.sign(string('hello world'))
+  assert.equal(toHex(sig), 'add3d1d105b59b51e258eb8d9a876288167d5b8aabe2577e81e9a67ce46a248b9d49da88d3cd73ceb9835c44d74ca28f081cf984c89f52ace33bca65ccf64e0c')
+  assert.ok(await keys.verify(sig, string('hello world')))
+  assert.end()
+})
