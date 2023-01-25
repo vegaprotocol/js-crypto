89d0c2e585b0494999a37ee423945726c63b62aa
Emil Bay <github@tixz.dk>
2023-01-25 01:47:52 +0100

    Add wallet id

--- test/vega-wallet.js
+++ test/vega-wallet.js
@@ -10,4 +10,14 @@ test('Can recreate wallet from seed', async assert => {
   const actual = await VegaWallet.fromSeed(seed)
 
   assert.deepEqual((await expected.keyPair(HARDENED + 1)).pk, (await actual.keyPair(HARDENED + 1)).pk)
+  assert.end()
+})
+
+test('vegawallet-desktop test vector', async assert => {
+  const mnemonic = 'swing ceiling chaos green put insane ripple desk match tip melt usual shrug turkey renew icon parade veteran lens govern path rough page render'
+  const wallet = await VegaWallet.fromMnemonic(mnemonic)
+
+  assert.equal('9df682a3c87d90567f260566a9c223ccbbb7529c38340cf163b8fe199dbf0f2e', wallet.id)
+
+  assert.end()
 })
